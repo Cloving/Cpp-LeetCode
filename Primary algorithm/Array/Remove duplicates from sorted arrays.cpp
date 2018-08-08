ccd5187a73b1658aac772a81dbf29d7f12c1c114
cloving <quds8724@qq.com>
2018-08-08 15:13:16 +0800

    refresh

---
+++ Primary algorithm/Array/Remove duplicates from sorted arrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        int len = nums.size();
+        if (len <= 1) {
+            return len; 
+        } else {
+            for (int j = 1; j < len; j++) {
+                if (nums[i] != nums[j]) {
+                    nums[++i] = nums[j];
+                }
+            }
+            return i+1;
+        }
+    }
+};
