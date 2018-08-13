21e8b818ad2a3a09f4101c0b016d7083aba01790
cloving <quds8724@qq.com>
2018-08-13 17:34:20 +0800

    refresh

---
+++ Primary algorithm/Array/Single Number.cpp
@@ -0,0 +1,15 @@
+/**
+ * 只出现一次的数字
+ * 两个相同的元素按位异或后为0，只剩下最后一个单独的数字
+ */
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+      int len = nums.size();
+      int value = nums[0];  
+      for(int i = 1; i < len; i++) {
+        value ^= nums[i]; 
+      }
+      return value;
+    }
+};
