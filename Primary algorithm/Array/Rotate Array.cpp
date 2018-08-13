29d2c5bf61e1531f74d1585de0c35ab8b23bf4f0
cloving <quds8724@qq.com>
2018-08-13 08:31:09 +0800

    旋转数组的第二种方法

--- Primary algorithm/Array/Rotate Array.cpp
+++ Primary algorithm/Array/Rotate Array.cpp
@@ -29,3 +29,33 @@ public:
     }
 };
 
+/**
+ * 第二种：
+ * 每一次都确定一个数组元素的新位置，
+ * 总次数为数组的长度
+ */
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int tempValue, cur;
+        int i = 0, start = 0, len = nums.size();
+        int s = len;
+        while (len < k) {
+            k = k-len;
+        }
+        cur = nums[start];
+        while (s--) {
+            i = (i+k)%len;
+            tempValue = nums[i];
+            nums[i] = cur;
+            // 2018-8-13：某个数组元素的新位置可能会在数组元素的开头
+            if (start == i) {
+                start++;
+                i++;
+                cur = nums[start];
+            } else {
+                cur = tempValue;
+            }
+        }
+    }
+};
