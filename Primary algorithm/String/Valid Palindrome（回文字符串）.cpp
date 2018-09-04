3ff02a9920dde7b9110a29c19ed2dc3cf536c985
cloving <quds8724@qq.com>
2018-09-04 19:29:05 +0800

    update

---
+++ Primary algorithm/String/Valid Palindrome（回文字符串）.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i= 0, j = s.length()-1;
+        while(i<j) {
+            if (!isalnum(s[i])) {
+                i++;
+            } else if (!isalnum(s[j])) {
+                j--;
+            } else if (toupper(s[i]) != toupper(s[j])) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+};
