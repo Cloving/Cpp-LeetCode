1d690826a79446b2e7c2ee710126dbbb1e6dd28a
cloving <quds8724@qq.com>
2018-09-12 10:50:55 +0800

    update: Linked list

---
+++ Primary algorithm/Linked list/Merge Two Sorted Lists （合并两个有序链表）.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL) {
+            return l2;
+        }
+        if (l2 == NULL) {
+            return l1;
+        }
+        ListNode* cur = new ListNode(-1);
+        ListNode* res = cur;
+        while (l1!= NULL && l2 != NULL) {
+            if (l1->val < l2->val) {
+                cur->next = l1;
+                l1 = l1->next;                
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        cur->next = (l1 == NULL)? l2:l1;
+        return res->next;
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL) {
+            return l2;
+        }
+        if(l2 == NULL) {
+            return l1;
+        }
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
