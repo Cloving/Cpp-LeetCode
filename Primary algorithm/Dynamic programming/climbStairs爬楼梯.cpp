c05f44f5c37612acffda4a3da1445e173fac01b0
cloving <quds8724@qq.com>
2018-09-19 11:17:04 +0800

    update

---
+++ Primary algorithm/Dynamic programming/climbStairs爬楼梯.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> res(n + 1, 1);
+        for (int i = 2; i <= n; ++i) {
+            res[i] = res[i - 1] + res[i - 2];
+        }
+        return res.back();
+    }
+};
+
+/* 递归也可以但是会超时 */
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+}
