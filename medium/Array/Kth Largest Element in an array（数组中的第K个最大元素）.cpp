453b6255841b4900322e9ae2cdfc94cf1e38376d
cloving <quds8724@qq.com>
2018-10-12 15:19:51 +0800

    UP

--- medium/Array/Kth Largest Element in an array（数组中的第K个最大元素）.cpp
+++ medium/Array/Kth Largest Element in an array（数组中的第K个最大元素）.cpp
@@ -27,9 +27,29 @@ public:
         int left = 0, right = nums.size()-1;
         while(true) {
           int pos = partition(nums, left, right);
-          if (pos = k-1) {
-            
+          if (pos == k-1) {
+            return nums[pos];
+          } else if (pos < k-1) {
+              left = pos + 1;
+          } else {
+              right = pos - 1;
           }
         }
+    }
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[left], l = left+1, r = right;
+        while (l <= r) {
+            if (nums[l] < pivot && nums[r] > pivot) {
+                swap(nums[l++], nums[r--]);
+            }
+            if (nums[l] >= pivot) {
+                l++;
+            }
+            if (nums[r] <= pivot) {
+                r--;
+            }
+        }
+        swap(nums[left], nums[r]);
+        return r;
     }
 };
