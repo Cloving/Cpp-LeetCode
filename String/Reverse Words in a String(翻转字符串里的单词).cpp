1d215b397dfe27d533d12aeaa07b319ec391a267
cloving <quds8724@qq.com>
2018-10-19 15:21:29 +0800

    update

--- String/Reverse Words in a String(翻转字符串里的单词).cpp
+++ String/Reverse Words in a String(翻转字符串里的单词).cpp
@@ -1,14 +1,52 @@
+/* Reverse Words in a String(翻转字符串里的单词) */
+
+/* 第一种 */
 class Solution {
 public:
     void reverseWords(string &s) {
-        reverse(s.begin(), s.end());
-        int len = s.size(), left = 0, right = 0, storeIndex = 0;
-        for (int i = 0; i < len; i++) {
-          while (s[i++] == ' ');
-          left = i - 1;
-          while (s[i++] != ' ');
-          right = i - 2;
-          reverse(s.begin()+left, s.begin()+right+1);
+      reverse(s.begin(), s.end());
+      int len = s.size(), cur = 0, storeIndex = 0;
+      for (int i = 0; i < len; i++) {
+        if (s[i] != ' ') {
+          if (storeIndex != 0) {
+            s[storeIndex++] = ' ';
+          }
+          cur = i;
+          while (cur < len && s[cur] != ' ') {
+            s[storeIndex++] = s[cur++];
+          }
+          reverse(s.begin()+storeIndex-(cur-i), s.begin()+storeIndex);
+          i = cur;
         }
+      }
+      s.resize(storeIndex);
+    }
+};
+
+
+/* 第二种 */
+class Solution {
+public:
+    void reverseWords(string &s) {
+      int i = 0, j = 0;
+      while (s[0] == ' ') {
+        s.erase(s.begin());
+      }
+      reverse(s.begin(), s.end());
+      while (s[0] == ' ') {
+        s.erase(s.begin());
+      }
+      while (j < s.size()) {
+        if (s[j] == ' ') {
+          reverse(s.begin() + i, s.begin() + j);
+          i = ++j;
+          while (j < s.size() && s[j] == ' ') {
+            s.erase(s.begin() + j);
+          }
+        } else {
+          j++;
+        }
+      }
+      reverse(s.begin() + i, s.begin() + j);
     }
 };
