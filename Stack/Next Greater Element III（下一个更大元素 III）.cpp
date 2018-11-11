d67478e2aee7e63c18d9ad4d2580773b5ddfb855
Cloving <quds8724@qq.com>
2018-11-11 22:59:15 +0800

    update

--- Stack/Next Greater Element III（下一个更大元素 III）.cpp
+++ Stack/Next Greater Element III（下一个更大元素 III）.cpp
@@ -2,7 +2,26 @@
 
 class Solution {
 public:
-    int nextGreaterElement(int n) {
-        
+  int nextGreaterElement(int n) {
+    string s = to_string(n);
+    int i = s.size() - 1;
+    while (i > 0) {
+      if (s[i] > s[i-1]) {
+        break;
+      }
+      i--;
     }
+    if (i == 0) {
+      return -1;
+    }
+    for (int j = s.size(); j > i-1; j--) {
+      if (s[j] > s[i-1]) {
+        swap(s[j], s[i-1]);
+        break;
+      }
+    }
+    sort(s.begin() + i, s.end());
+    long long res = stoll(s);
+    return res > INT_MAX ? -1 : res;
+  }
 };
